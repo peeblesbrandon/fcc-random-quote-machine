1fa0d6cb44bf3366849141af4c60be7b095d8427
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-10 12:59:32 -0400

    remove unnecessary escape and add rel

--- src/components/QuoteBox/QuoteBox.js
+++ src/components/QuoteBox/QuoteBox.js
@@ -27,14 +27,14 @@ class QuoteBox extends React.Component {
     }
     // render the final quote box
     render () {
-        const tweetURL = `https://twitter.com/intent/tweet?text=\"${this.state.quote}\" - ${this.state.author}`;
+        const tweetURL = `https://twitter.com/intent/tweet?text="${this.state.quote}" - ${this.state.author}`;
         return (
             <div id="container" className="container-xs d-flex justify-content-center align-items-center vh-100 bg-info">
                 <div id="quote-box" className="col-10 p-4 d-flex flex-column text-info">
                     <h1 id="text" className="d-block text-left">"{this.state.quote}"</h1>
                     <p id="author" className="align-self-end font-italic">- {this.state.author}</p>
                     <div className="d-flex justify-content-between">
-                        <a className="btn btn-info font-weight-bold" id="tweet-quote" target="_blank" href={tweetURL}><i className="fa fa-twitter" /> Share this quote</a>
+                        <a className="btn btn-info font-weight-bold" id="tweet-quote" rel="noopener noreferrer" target="_blank" href={tweetURL}><i className="fa fa-twitter" /> Share this quote</a>
                         <button onClick={this.handleClick} className="btn btn-info font-weight-bold" variant="light" id="new-quote"><i className="fa fa-random" /> New quote</button>
                     </div>
                 </div>
