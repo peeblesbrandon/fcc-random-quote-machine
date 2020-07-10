e1e7135eed8f65ae8cd2a2d3e98b66bb502f0797
Brandon Peebles <peeblesbrandon@gmail.com>
2020-07-10 12:50:22 -0400

    fix buttons and tweet URL

--- src/components/QuoteBox/QuoteBox.js
+++ src/components/QuoteBox/QuoteBox.js
@@ -27,14 +27,15 @@ class QuoteBox extends React.Component {
     }
     // render the final quote box
     render () {
+        const tweetURL = `https://twitter.com/intent/tweet?text=\"${this.state.quote}\" - ${this.state.author}`;
         return (
             <div id="container" className="container-xs d-flex justify-content-center align-items-center vh-100 bg-info">
                 <div id="quote-box" className="col-10 p-4 d-flex flex-column text-info">
                     <h1 id="text" className="d-block text-left">"{this.state.quote}"</h1>
                     <p id="author" className="align-self-end font-italic">- {this.state.author}</p>
                     <div className="d-flex justify-content-between">
-                        <a className="btn btn-info" id="tweet-quote" href="www.twitter.com/intent/tweet"><i className="fa fa-twitter fa-2x" /></a>
-                        <button onClick={this.handleClick} className="btn btn-info font-weight-bold" variant="light" id="new-quote">New quote</button>
+                        <a className="btn btn-info font-weight-bold" id="tweet-quote" target="_blank" href={tweetURL}><i className="fa fa-twitter" /> Share this quote</a>
+                        <button onClick={this.handleClick} className="btn btn-info font-weight-bold" variant="light" id="new-quote"><i className="fa fa-random" /> New quote</button>
                     </div>
                 </div>
             </div>  
